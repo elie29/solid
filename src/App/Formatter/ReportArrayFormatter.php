fbd68c5fd94faf5f824c056faf99b597042c8ef5
Elie NEHME <elie29@gmail.com>
2018-02-21 13:16:41 +0100

    Added LSP

--- src/App/Formatter/ReportArrayFormatter.php
+++ src/App/Formatter/ReportArrayFormatter.php
@@ -5,18 +5,13 @@ namespace App\Formatter;
 class ReportArrayFormatter extends ReportFormatter
 {
 
-    /**
-     * Liskov violation: format returns array instead of a string.
-     *
-     * @return array
-     */
     public function format(array $data)
     {
         $result = array();
         foreach ($data as $k => $v){
             $result[] = "$k => $v";
         }
-        return $result;
+        return parent::format($result);
     }
 
 }
